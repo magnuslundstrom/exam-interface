e2d3ba5e5d7036c12de621d499aa166cb76e0457
magnuslundstrom <magnuslundstromdk@gmail.com>
2020-11-21 12:33:40 +0100

    Revert "Basic setup improved"
    This reverts commit feb002159aea62e3d0876aad7d30ebb3c14ced8e.

--- index.php
+++ index.php
@@ -1,20 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<?php
-$metaTitle = 'Sign up to iHub';
-require_once __DIR__ . '/components/head.php';
-?>
-
-
-<section class="auth-form">
-    <h1>Adam's branch</h1>
-    <form>
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button>Sign up</button>
-    </form>
-    <div>Already have an account? <a href="login.html">Log in!</a></div>
-</section>
-
-<?php require_once __DIR__ . '/components/footer.php';?>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Sign up</title>
+        <link rel="stylesheet" href="./styles/app.css" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Varela+Round&display=swap"
+            rel="stylesheet"
+        />
+    </head>
+    <body>
+        <section class="auth-form">
+            <h1>Adam's branch</h1>
+            <form>
+                <input type="text" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button>Sign up</button>
+            </form>
+            <div>Already have an account? <a href="login.html">Log in!</a></div>
+        </section>
+        <script src="app.js"></script>
+    </body>
+</html>
