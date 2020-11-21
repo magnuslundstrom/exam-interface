9a7b4b34fb17a9045a2ad5d2a2761e0405dcee66
magnuslundstrom <magnuslundstromdk@gmail.com>
2020-11-21 15:16:15 +0100

    Base header component

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,16 @@
+const burgerMenu = document.querySelector('.burger-menu');
+const closeMenu = document.querySelector('.close-menu');
+const menuOverlay = document.querySelector('.menu-overlay');
+const menu = document.querySelector('.menu');
+const backArrow = document.querySelector('.back-arrow');
 
+function toggleMenu() {
+    menuOverlay.classList.toggle('hidden');
+}
+
+burgerMenu.addEventListener('click', toggleMenu);
+closeMenu.addEventListener('click', toggleMenu);
+menuOverlay.addEventListener('click', toggleMenu);
+menu.addEventListener('click', (e) => e.stopPropagation());
+
+if (backArrow) backArrow.addEventListener('click', () => history.back());
