07c4feb9e793f024ee2c95f8bf79e403d2213af8
magnuslundstrom <magnuslundstromdk@gmail.com>
2020-11-21 12:33:51 +0100

    Revert "Revert "Basic setup improved""
    This reverts commit e2d3ba5e5d7036c12de621d499aa166cb76e0457.

---
+++ components/head.php
@@ -0,0 +1,18 @@
+<?php
+// Please define $metaTitle above your require_once of this component
+$metaTitle = $metaTitle ?? 'iHub';
+?>
+
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?=$metaTitle?></title>
+    <link rel="stylesheet" href="./styles/app.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Varela+Round&display=swap"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+
+<body>
