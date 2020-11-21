e9ab9253e9642daa5aedf371caead71b3e17529e
magnussony <magnuslundstromdk@gmail.com>
2020-11-21 11:24:48 +0100

    initial

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Sign up</title>
+        <link rel="stylesheet" href="./styles/app.css" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Varela+Round&display=swap"
+            rel="stylesheet"
+        />
+    </head>
+    <body>
+        <section class="auth-form">
+            <h1>Sign up</h1>
+            <form>
+                <input type="text" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button>Sign up</button>
+            </form>
+            <div>Already have an account? <a href="login.html">Log in!</a></div>
+        </section>
+        <script src="app.js"></script>
+    </body>
+</html>
