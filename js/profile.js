77120bb13e97b4d22dd11b32aa1b798dace14c61
magnuslundstrom <magnuslundstromdk@gmail.com>
2020-11-23 11:09:37 +0100

    Profile page

---
+++ js/profile.js
@@ -0,0 +1,63 @@
+if (document.querySelector('.profile')) {
+    (() => {
+        let passwordToggle = true;
+        // page wrapper
+        const profileHtml = document.querySelector('.profile');
+        // list of the editable targets
+        const profileDetailsList = document.querySelectorAll('.profile-detail');
+        // password paragraph
+        const passwordHtml = document.querySelector('.profile-detail-password');
+        // getting textContent from password paragraph
+        let actualPassword = passwordHtml.textContent;
+
+        // display or hide password
+        const togglePassword = () => {
+            if (passwordToggle) {
+                // replacing all characters with circle icon
+                const regex = /./gi;
+                passwordHtml.innerHTML = actualPassword.replaceAll(
+                    regex,
+                    '<i class="fas fa-circle"></i>'
+                );
+            } else {
+                // or sets the textContent to actual password variable
+                passwordHtml.textContent = actualPassword;
+            }
+            passwordToggle = !passwordToggle;
+        };
+        togglePassword();
+
+        profileHtml
+            .querySelector('.display-password')
+            .addEventListener('click', togglePassword);
+
+        const toggleView = (p, input) => {
+            p.classList.toggle('hidden');
+            input.classList.toggle('hidden');
+        };
+
+        // loop over each target and setup listeners for btns and inputs
+        profileDetailsList.forEach((element) => {
+            const editBtn = element.querySelector('.profile-edit');
+            const editInput = element.querySelector('input');
+            const p = element.querySelector('p');
+            const input = element.querySelector('input');
+
+            // bind listener to edit button
+            editBtn.addEventListener('click', (e) => {
+                toggleView(p, input);
+            });
+
+            // bind listener to input
+            editInput.addEventListener('keyup', (e) => {
+                if (e.target.id === 'password-input') {
+                    if (!togglePassword) {
+                        togglePassword();
+                    }
+                    actualPassword = e.target.value;
+                }
+                p.textContent = e.target.value;
+            });
+        });
+    })();
+}
